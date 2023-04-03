c3e404416dfe713b8953a9889487e289afe468a8
Tsuyoshi Tokuda <108762+tokuda109@users.noreply.github.com>
2023-04-03 09:53:00 +0900

    create nuxt module from template
    * add playground-nuxt2

---
+++ playground-nuxt2/nuxt.config.ts
@@ -0,0 +1,6 @@
+import { defineNuxtConfig } from '@nuxt/bridge';
+
+export default defineNuxtConfig({
+  modules: ['../src/module'],
+  telemetry: false,
+});
---
+++ playground-nuxt2/pages/index.vue
@@ -0,0 +1,8 @@
+<template>
+  <div>
+    Nuxt module playground! (Nuxt 2)
+  </div>
+</template>
+
+<script setup>
+</script>
